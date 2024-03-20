b0ab673437e6955a9d1a4d509e652152147b94ad
bananky <wojnarskap@gmail.com>
2024-03-20 21:30:09 +0100

    commit2

--- zadanie1/paradygmaty.pas
+++ zadanie1/paradygmaty.pas
@@ -1,18 +1,47 @@
-program GenerateRandomNumbers;
+program GenerateAndSortRandomNumbers;
 uses SysUtils;
 
-procedure GenerateAndPrintRandomNumbers;
+const
+  NumberOfNumbers = 50; 
+
 var
-  i, number: Integer;
+  Numbers: array[1..NumberOfNumbers] of Integer; 
+
+procedure GenerateRandomNumbers;
+var
+  i: Integer;
 begin
   Randomize; 
-  for i := 1 to 50 do
+  for i := 1 to NumberOfNumbers do
   begin
-    number := Random(101); 
-    WriteLn(number);
+    Numbers[i] := Random(101); 
   end;
 end;
 
+procedure SortNumbers;
+var
+  i, j, temp: Integer;
 begin
-  GenerateAndPrintRandomNumbers;
+  for i := 1 to NumberOfNumbers - 1 do
+    for j := 1 to NumberOfNumbers - i do
+      if Numbers[j] > Numbers[j + 1] then
+      begin
+        temp := Numbers[j];
+        Numbers[j] := Numbers[j + 1];
+        Numbers[j + 1] := temp;
+      end;
+end;
+
+procedure PrintNumbers;
+var
+  i: Integer;
+begin
+  for i := 1 to NumberOfNumbers do
+    WriteLn(Numbers[i]);
+end;
+
+begin
+  GenerateRandomNumbers;
+  SortNumbers; 
+  PrintNumbers; 
 end.
