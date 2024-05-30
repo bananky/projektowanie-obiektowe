5d7cc0fcb688d6f7d7a933424e0100414a2cf95c
bananky <wojnarskap@gmail.com>
2024-05-30 17:13:48 +0200

    commit1

---
+++ zadanie6/zadanie5/go.mod
@@ -0,0 +1,3 @@
+module zadanie5
+
+go 1.22.2
---
+++ zadanie6/zadanie5/src/components/Products.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+const Products = ({ addToCart }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/products');
+      if (!response.ok) {
+        throw new Error('Failed to fetch products');
+      }
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleAddToCart = (product) => {
+    addToCart(product);
+  };
+
+  return (
+    <div>
+      <h2>Lista Produktów</h2>
+      <ul className="product-list">
+        {products.map(product => (
+          <li key={product.id} className="product-item">
+            {product.name} - {product.price} zł
+            <button onClick={() => handleAddToCart(product)}>Dodaj do koszyka</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Products;
